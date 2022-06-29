3221349748fde3458b3f4038ca68e4084296cbd3
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-06-29 13:23:22 +0100

    Add day 2 task

---
+++ src/main/java/com/dufuna/berlin/imodupsy/address/AddressApp.java
@@ -0,0 +1,12 @@
+package com.dufuna.berlin.imodupsy.address;
+
+/**
+ * @author iModupsy
+ * @created 29/06/2022
+ */
+public class AddressApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
